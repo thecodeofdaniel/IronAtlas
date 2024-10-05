eaa1dfca14cd2b4347b8258ff16d66021249ac17
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-10-05 04:18:14 -0700

    Add ability to open and close rows

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Pressable } from 'react-native';
 import DraggableFlatList, {
   RenderItemParams,
 } from 'react-native-draggable-flatlist';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import clsx from 'clsx';
+import { Ionicons } from '@expo/vector-icons';
 
 type Item = {
   id: number;
   title: string;
   parentId: number | null;
   order: number;
+  isOpen: boolean;
   children: Item[];
 };
 
@@ -20,18 +22,21 @@ const items: Item[] = [
     title: 'Hello',
     parentId: null,
     order: 0,
+    isOpen: true,
     children: [
       {
         id: 2,
         title: 'World',
         parentId: 1,
         order: 0,
+        isOpen: false,
         children: [
           {
             id: 4,
             title: 'Again',
             parentId: 2,
             order: 0,
+            isOpen: false,
             children: [],
           },
           {
@@ -39,6 +44,7 @@ const items: Item[] = [
             title: 'Too',
             parentId: 2,
             order: 1,
+            isOpen: false,
             children: [],
           },
         ],
@@ -50,6 +56,7 @@ const items: Item[] = [
     title: 'Goodbye',
     parentId: null,
     order: 1,
+    isOpen: false,
     children: [],
   },
 ];
@@ -61,6 +68,11 @@ type TreeProps = {
 
 const Tree = ({ items, level = 0 }: TreeProps) => {
   const [dataList, setDataList] = useState<Item[]>(items);
+  const [isOpen, setIsOpen] = useState(() => {
+    return items.map((item) => {
+      return item.isOpen;
+    });
+  });
 
   const RenderItem = ({
     item,
@@ -68,16 +80,45 @@ const Tree = ({ items, level = 0 }: TreeProps) => {
     isActive,
     getIndex, // Include getIndex
   }: RenderItemParams<Item>) => {
+    const currentIndex = getIndex();
+
     return (
       <TouchableOpacity
         activeOpacity={1}
         onLongPress={drag}
         disabled={isActive}
-        className={clsx('p-2 my-[1]', {
+        className={clsx('p-2 my-[1] flex flex-row', {
           'bg-red-500': isActive,
           'bg-blue-500': !isActive,
         })}
       >
+        {item.children.length > 0 && (
+          <Pressable
+            onPress={() =>
+              setIsOpen((prev) => {
+                const index = getIndex()!; // Get the index where you want to change the value
+                // Create a new array based on the previous state
+                const newBoolArr = [...prev];
+                // console.log('before', newBoolArr);
+                // Toggle the value at the specified index
+                newBoolArr[index] = !prev[index];
+                // console.log('after', newBoolArr);
+                return newBoolArr; // Return the new array
+              })
+            }
+          >
+            <Ionicons
+              name={
+                isOpen[currentIndex!]
+                  ? 'chevron-down'
+                  : 'chevron-forward-outline'
+              }
+              size={18}
+              style={{ marginRight: 12 }}
+              color={'white'}
+            />
+          </Pressable>
+        )}
         <Text className="text-green-500">{item.title}</Text>
       </TouchableOpacity>
     );
@@ -97,7 +138,7 @@ const Tree = ({ items, level = 0 }: TreeProps) => {
               isActive={isActive}
               getIndex={getIndex}
             />
-            {item.children.length > 0 && (
+            {item.children.length > 0 && isOpen[getIndex()!] && (
               <Tree items={item.children} level={level + 1} />
             )}
           </View>
