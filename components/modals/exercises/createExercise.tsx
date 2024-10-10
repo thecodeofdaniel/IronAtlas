dc02aae446b20f2fbc07ee675f776d58280fac6f
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-10-09 21:13:20 -0700

    Add dropdown picker to create exercise

--- components/modals/exercises/createExercise.tsx
+++ components/modals/exercises/createExercise.tsx
@@ -5,15 +5,48 @@ import { View, Text, TextInput, Button } from 'react-native';
 import { ModalData } from '@/store/modalStore';
 import { Stack, useRouter } from 'expo-router';
 import { useExerciseStore } from '@/store/exerciseStore';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useTagTreeStoreWithSetter } from '@/store/tagTreeStore';
+import { formatTag } from '@/utils/utils';
 
 type Props = {
   modalData: ModalData['createExercise'];
   closeModal: () => void;
 };
 
+type TagItem = {
+  label: string;
+  value: string;
+  selectable: boolean;
+  disabled: boolean;
+};
+
 export default function CreateExercise({ modalData, closeModal }: Props) {
   const createExercise = useExerciseStore((state) => state.createExercise);
+  const { tagMap, setter } = useTagTreeStoreWithSetter();
   const [name, setName] = useState('');
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  // const [items, setItems] = useState([
+  //   { label: 'Apple', value: 'apple' },
+  //   { label: 'Banana', value: 'banana' },
+  //   { label: 'Pear', value: 'pear' },
+  // ]);
+  const [items, setItems] = useState(() => {
+    const arr: TagItem[] = [];
+
+    Object.values(tagMap).forEach((tag) => {
+      arr.push({
+        label: tag.title,
+        value: formatTag(tag.title),
+        selectable: true,
+        disabled: false,
+      });
+    });
+
+    return arr;
+  });
+
   const router = useRouter();
 
   const addExercise = () => {
@@ -31,6 +64,8 @@ export default function CreateExercise({ modalData, closeModal }: Props) {
     closeModal();
     router.back();
   };
+
+  const handleSelect = (items: TagItem) => {};
 
   return (
     <>
@@ -57,6 +92,24 @@ export default function CreateExercise({ modalData, closeModal }: Props) {
             onPress={addExercise}
             disabled={name.trim() === ''}
           />
+        </View>
+        <View className="flex-1">
+          <View className="flex-1 items-center justify-center px-4">
+            <DropDownPicker
+              multiple={true}
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              // setItems={setItems}
+              placeholder={'Choose a fruit.'}
+              disabledItemLabelStyle={{ opacity: 0.2 }}
+            />
+          </View>
+          <View className="flex-1 items-center justify-center">
+            <Text>Chosen fruit: {value === null ? 'none' : value}</Text>
+          </View>
         </View>
       </View>
     </>
