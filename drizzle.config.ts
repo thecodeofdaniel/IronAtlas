3ed19a1817562a6d6597fc071d6e4f19e1c79785
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-10-16 18:56:16 -0700

    Add config files for drizzle to work

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  out: './db/drizzle',
+  dialect: 'sqlite',
+  driver: 'expo', // <--- very important
+});
