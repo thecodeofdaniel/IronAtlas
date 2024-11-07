fec16b4956a7eb90aae2b39c5e75579954bb69a1
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-11-06 22:04:00 -0800

    Improve template component

--- app/(tabs)/workout/template.tsx
+++ app/(tabs)/workout/template.tsx
@@ -106,9 +106,8 @@ const UnderlayLeft = ({
 
   return (
     <Animated.View
-      // style={[styles.row, styles.underlayLeft, animStyle]} // Fade in on open
       style={[animStyle]}
-      className="flex-1 flex-row items-center justify-end bg-red-500 pr-4"
+      className="my-[1] flex-1 flex-row items-center justify-end bg-red-500 pr-4"
     >
       <TouchableOpacity onPress={onPressDelete}>
         <Text className="text-2xl font-bold text-white">Delete</Text>
@@ -124,115 +123,17 @@ function TemplateTree({
   templateChildren,
   level,
 }: TemplateProps) {
-  // const RenderItem = ({
-  //   item: group,
-  //   drag,
-  //   isActive,
-  //   getIndex,
-  // }: RenderItemParams<TemplateObj>) => {
-  //   const RenderSet = ({ reps, type }: SettType) => {
-  //     const [text, setText] = useState(`${reps}${type}`);
-
-  //     return (
-  //       <View className="my-1 mr-1 flex flex-row gap-1">
-  //         {/* <Text>{reps}</Text>
-  //       <Text>{type}</Text> */}
-  //         <TextInput value={text} onChangeText={(text) => setText(text)} />
-  //       </View>
-  //     );
-  //   };
-
-  //   if (group.parentId === null) {
-  //     return null;
-  //   }
-
-  //   const itemRefs = useRef(new Map());
-
-  //   return (
-  //     <>
-  //       <TouchableOpacity
-  //         onLongPress={level > 0 ? drag : undefined}
-  //         disabled={isActive}
-  //         activeOpacity={1}
-  //         className={clsx('my-[1] flex flex-row items-center p-2', {
-  //           'bg-red-500': isActive,
-  //           'bg-blue-800': !isActive,
-  //         })}
-  //       >
-  //         <Text className="text-white">
-  //           {exerciseMap[group.exerciseId].label}
-  //         </Text>
-  //       </TouchableOpacity>
-  //       {/* {group.exerciseId > 0 && (
-  //         <>
-  //           <Text>Exercise Info</Text>
-  //           <FlatList
-  //             horizontal
-  //             data={group.sets}
-  //             keyExtractor={(item, index) =>
-  //               `${group.exerciseId}-${index}-${item.reps}`
-  //             }
-  //             renderItem={({ item }) => (
-  //               <RenderSet reps={item.reps} type={item.type} />
-  //             )}
-  //           />
-  //           <Pressable className="rounded-md border bg-stone-400">
-  //             <Text className="text-center text-white">Add set</Text>
-  //           </Pressable>
-  //         </>
-  //       )} */}
-  //     </>
-  //   );
-  // };
-
   const RenderItem = (params: RenderItemParams<TemplateObj>) => {
-    const { item: item, isActive, drag } = params;
-
-    if (item.parentId === null) {
-      return null;
+    if (params.item.parentId === null) {
+      return null; // This is for the root node
     }
 
     const itemRefs = useRef(new Map());
-
-    // return (
-    //   <>
-    //     <TouchableOpacity
-    //       onLongPress={level > 0 ? drag : undefined}
-    //       disabled={isActive}
-    //       activeOpacity={1}
-    //       className={clsx('my-[1] flex flex-row items-center p-2', {
-    //         'bg-red-500': isActive,
-    //         'bg-blue-800': !isActive,
-    //       })}
-    //     >
-    //       <Text className="text-white">
-    //         {exerciseMap[group.exerciseId].label}
-    //       </Text>
-    //     </TouchableOpacity>
-    //   </>
-    // );
-
-    // return (
-    //   <View key={item.exerciseId} style={{ paddingLeft: 5 * level }}>
-    //     <RowItem {...params} itemRefs={itemRefs} />
-    //     {item.children.length > 0 && (
-    //       <TemplateTree
-    //         templateMap={templateMap}
-    //         exerciseMap={exerciseMap}
-    //         reorderTemplate={reorderTemplate}
-    //         templateChildren={item.children}
-    //         level={level + 1}
-    //       />
-    //     )}
-    //   </View>
-    // );
 
     return <RowItem {...params} itemRefs={itemRefs} />;
   };
 
   const templateExercises = templateChildren.map((id) => templateMap[id]);
-
-  console.log(`${level}: ${JSON.stringify(templateExercises)}`);
 
   return (
     <>
@@ -240,31 +141,9 @@ function TemplateTree({
         data={templateExercises}
         onDragEnd={({ data }) => reorderTemplate(data)}
         keyExtractor={(item) => item.uuid}
-        // renderItem={RenderItem}
-        // renderItem={({ item, drag, isActive, getIndex }) => {
-        //   return (
-        //     <View key={item.exerciseId} style={{ paddingLeft: 5 * level }}>
-        //       <RenderItem
-        //         item={item}
-        //         drag={drag}
-        //         isActive={isActive}
-        //         getIndex={getIndex}
-        //       />
-        //       {item.children.length > 0 && (
-        //         <TemplateTree
-        //           templateMap={templateMap}
-        //           exerciseMap={exerciseMap}
-        //           reorderTemplate={reorderTemplate}
-        //           templateChildren={item.children}
-        //           level={level + 1}
-        //         />
-        //       )}
-        //     </View>
-        //   );
-        // }}
         renderItem={(params) => {
           return (
-            <View style={{ paddingLeft: 5 * level }}>
+            <View style={{ paddingLeft: 10 * (level - 1) }}>
               <RenderItem {...params} />
               {params.item.children.length > 0 && (
                 <TemplateTree
@@ -284,33 +163,22 @@ function TemplateTree({
 }
 
 export default function TemplateScreen() {
-  const [templateMap, setTemplateMap] = useState({});
   const { template, reorderTemplate } = useWorkoutStore((state) => state);
   const { exerciseMap } = useExerciseStore((state) => state);
 
-  console.log('Template:', template);
-
   return (
     <>
-      <View className="flex-1 justify-between border">
-        <GestureHandlerRootView>
-          <TemplateTree
-            templateMap={template}
-            exerciseMap={exerciseMap}
-            reorderTemplate={reorderTemplate}
-            templateChildren={['0']}
-            level={0}
-          />
-        </GestureHandlerRootView>
-        <View className="flex flex-row gap-2">
-          <Pressable className="flex-1 border bg-stone-300">
-            <Text className="text-center">Add group</Text>
-          </Pressable>
-          <Pressable className="flex-1 border bg-stone-300">
-            <Text className="text-center">Add exercise</Text>
-          </Pressable>
-        </View>
-      </View>
+      <GestureHandlerRootView
+        style={{ borderColor: 'black', borderWidth: 1, flex: 1 }}
+      >
+        <TemplateTree
+          templateMap={template}
+          exerciseMap={exerciseMap}
+          reorderTemplate={reorderTemplate}
+          templateChildren={['0']}
+          level={0}
+        />
+      </GestureHandlerRootView>
     </>
   );
 }
