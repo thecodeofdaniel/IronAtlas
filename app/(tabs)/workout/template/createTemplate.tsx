8e8275c10f0654bcfab19ab068d539b38ec500f1
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-11-13 01:44:45 -0800

    Implement logic for creating a template

--- app/(tabs)/workout/template/createTemplate.tsx
+++ app/(tabs)/workout/template/createTemplate.tsx
@@ -1,9 +1,5 @@
-import {
-  useTemplateStore,
-  useTemplateStoreHook,
-} from '@/store/template/templateStore';
 import { Stack, useRouter } from 'expo-router';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -14,32 +10,73 @@ import {
 } from 'react-native';
 import TemplateScreen2 from '../../../../components/Template/Template2';
 import { useModalStore } from '@/store/modalStore';
-import { useExerciseSelectionHook } from '@/store/exerciseSelection/exerciseSelectionHook';
 import { useWorkoutStoreHook } from '@/store/workout/workoutStore';
-import TemplateScreen from '../Template';
 import {
   GestureHandlerRootView,
   TextInput,
 } from 'react-native-gesture-handler';
-import { Controller, useForm } from 'react-hook-form';
 
-export default function CreateTemplate() {
+const emptyErrorMsgs = {
+  templateName: '',
+  templateInfo: '',
+  exercises: '',
+};
+
+export default function CreateTemplate2() {
   const router = useRouter();
   const { template, actions } = useWorkoutStoreHook();
   const openModal = useModalStore((state) => state.openModal);
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      templateName: '',
-      templateInfo: '',
-    },
-  });
+  const [templateName, setTemplateName] = useState('');
+  const [templateInfo, setTemplateInfo] = useState('');
+  const [errorMsgs, setErrorMsgs] = useState(emptyErrorMsgs);
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = () => {
+    // Reset messages
+    setErrorMsgs(emptyErrorMsgs);
+
+    let hasError = false;
+
+    // Template must have a name
+    if (templateName === '') {
+      setErrorMsgs((prev) => ({
+        ...prev,
+        templateName: 'Must include a name',
+      }));
+      hasError = true;
+    }
+
+    // A template must include at least one exercise
+    if (template[0].children.length === 0) {
+      setErrorMsgs((prev) => ({
+        ...prev,
+        exercises: 'Must include one exercise',
+      }));
+      hasError = true;
+    }
+
+    // A superset cannot be empty
+    template[0].children.forEach((exerciseUUID) => {
+      if (
+        template[exerciseUUID].exerciseId === null &&
+        template[exerciseUUID].children.length === 0
+      ) {
+        setErrorMsgs((prev) => ({
+          ...prev,
+          exercises: 'A superset cannot be empty',
+        }));
+      }
+      hasError = true;
+    });
+
+    if (hasError) {
+      console.log('Has an error');
+      return;
+    }
+
+    console.log('Success');
+    return;
+  };
 
   return (
     <>
@@ -50,9 +87,9 @@ export default function CreateTemplate() {
           headerRight: () => (
             <Pressable
               className="rounded-md bg-green-500 px-4 py-2"
-              onPress={handleSubmit(onSubmit)}
+              onPress={onSubmit}
             >
-              <Text className="font-semibold text-white">Save</Text>
+              <Text className="font-semibold text-white">Savee</Text>
             </Pressable>
           ),
         }}
@@ -64,54 +101,35 @@ export default function CreateTemplate() {
         >
           <GestureHandlerRootView style={{ flex: 1, gap: 4 }}>
             <View className="flex flex-col">
-              <Controller
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <>
-                    <Text className="text-lg font-medium">Template Name</Text>
-                    <TextInput
-                      onBlur={onBlur}
-                      onChangeText={onChange}
-                      value={value}
-                      className="border px-1 py-2"
-                    />
-                  </>
-                )}
-                name="templateName"
-              />
-              {errors.templateName && (
-                <Text className="text-red-500">This is required.</Text>
+              <View>
+                <Text className="text-lg font-medium">Template Name</Text>
+                <TextInput
+                  onChangeText={(text) => setTemplateName(text)}
+                  value={templateName}
+                  className="border px-1 py-2"
+                />
+              </View>
+              {errorMsgs.templateName && (
+                <Text className="text-red-500">
+                  Add a name to this template
+                </Text>
               )}
-              <Controller
-                control={control}
-                rules={{
-                  required: false,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <>
-                    <Text className="text-lg font-medium">
-                      Template Details
-                    </Text>
-                    <TextInput
-                      className="border px-1 py-2"
-                      style={{ minHeight: 50 }} // Optional: ensures minimum height
-                      multiline={true}
-                      numberOfLines={2}
-                      textAlignVertical="top" // This helps on Android
-                      maxLength={100}
-                      onSubmitEditing={() => Keyboard.dismiss()}
-                      onChangeText={onChange}
-                      onBlur={onBlur}
-                      value={value}
-                    />
-                  </>
-                )}
-                name="templateInfo"
-              />
-              {errors.templateInfo && (
+              <View>
+                <Text className="text-lg font-medium">Template Details</Text>
+                <TextInput
+                  className="border px-1 py-2"
+                  style={{ minHeight: 50 }} // Optional: ensures minimum height
+                  multiline={true}
+                  numberOfLines={2}
+                  textAlignVertical="top" // This helps on Android
+                  maxLength={100}
+                  onSubmitEditing={() => Keyboard.dismiss()}
+                  onChangeText={(text) => setTemplateInfo(text)}
+                  value={templateInfo}
+                />
+              </View>
+
+              {errorMsgs.templateInfo && (
                 <Text className="text-red-500">This is required.</Text>
               )}
             </View>
@@ -119,7 +137,9 @@ export default function CreateTemplate() {
             <View className="flex-1 border">
               <TemplateScreen2 template={template} actions={actions} />
             </View>
-            <Text>Select at least one exercise or superset</Text>
+            {errorMsgs.exercises && (
+              <Text className="text-red-500">{errorMsgs.exercises}</Text>
+            )}
           </GestureHandlerRootView>
         </TouchableWithoutFeedback>
         <Pressable
