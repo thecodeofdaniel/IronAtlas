be4fb4c5322615fca09a120f7ed48b552d89dc0a
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-11-12 23:41:20 -0800

    Add react-hook-form package and use it for creating templates

--- app/(tabs)/workout/template/createTemplate.tsx
+++ app/(tabs)/workout/template/createTemplate.tsx
@@ -10,6 +10,7 @@ import {
   Pressable,
   Keyboard,
   TouchableWithoutFeedback,
+  Button,
 } from 'react-native';
 import TemplateScreen2 from '../../../../components/Template/Template2';
 import { useModalStore } from '@/store/modalStore';
@@ -20,13 +21,25 @@ import {
   GestureHandlerRootView,
   TextInput,
 } from 'react-native-gesture-handler';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function CreateTemplate() {
   const router = useRouter();
   const { template, actions } = useWorkoutStoreHook();
   const openModal = useModalStore((state) => state.openModal);
 
-  console.log('Template', template);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      templateName: '',
+      templateInfo: '',
+    },
+  });
+
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <>
@@ -37,10 +50,7 @@ export default function CreateTemplate() {
           headerRight: () => (
             <Pressable
               className="rounded-md bg-green-500 px-4 py-2"
-              onPress={() => {
-                openModal('saveTemplate');
-                router.push('/modal');
-              }}
+              onPress={handleSubmit(onSubmit)}
             >
               <Text className="font-semibold text-white">Save</Text>
             </Pressable>
@@ -54,28 +64,66 @@ export default function CreateTemplate() {
         >
           <GestureHandlerRootView style={{ flex: 1, gap: 4 }}>
             <View className="flex flex-col">
-              <Text className="text-lg font-medium">Template Name</Text>
-              <TextInput className="border px-1 py-2" />
-              <Text className="text-lg font-medium">Template Details</Text>
-              <TextInput
-                className="border px-1 py-2"
-                multiline={true}
-                numberOfLines={2}
-                textAlignVertical="top" // This helps on Android
-                placeholder="Enter template description..."
-                style={{ minHeight: 50 }} // Optional: ensures minimum height
-                maxLength={100}
-                onSubmitEditing={() => Keyboard.dismiss()}
+              <Controller
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <>
+                    <Text className="text-lg font-medium">Template Name</Text>
+                    <TextInput
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                      className="border px-1 py-2"
+                    />
+                  </>
+                )}
+                name="templateName"
               />
+              {errors.templateName && (
+                <Text className="text-red-500">This is required.</Text>
+              )}
+              <Controller
+                control={control}
+                rules={{
+                  required: false,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <>
+                    <Text className="text-lg font-medium">
+                      Template Details
+                    </Text>
+                    <TextInput
+                      className="border px-1 py-2"
+                      style={{ minHeight: 50 }} // Optional: ensures minimum height
+                      multiline={true}
+                      numberOfLines={2}
+                      textAlignVertical="top" // This helps on Android
+                      maxLength={100}
+                      onSubmitEditing={() => Keyboard.dismiss()}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                      value={value}
+                    />
+                  </>
+                )}
+                name="templateInfo"
+              />
+              {errors.templateInfo && (
+                <Text className="text-red-500">This is required.</Text>
+              )}
             </View>
             <Text className="text-lg font-medium">Exercises</Text>
             <View className="flex-1 border">
               <TemplateScreen2 template={template} actions={actions} />
             </View>
+            <Text>Select at least one exercise or superset</Text>
           </GestureHandlerRootView>
         </TouchableWithoutFeedback>
         <Pressable
-          className="bg-blue-500 p-4"
+          className="mt-2 bg-blue-500 p-4"
           onPress={() => {
             openModal('selectExercises', {
               isSuperset: false,
