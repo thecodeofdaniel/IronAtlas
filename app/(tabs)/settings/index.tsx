7adc8d3b9ef033711ab5ceb31707d7cd2a2e5acb
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-11-25 06:21:06 -0800

    Add button to set theme

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
+import { useThemeContext } from '@/store/context/themeContext';
 
 export default function SettingsTab() {
+  const { themeName, setTheme } = useThemeContext();
   return (
     <>
       <Stack.Screen options={{ title: 'Settings' }} />
-      <View>
-        <Text>index</Text>
+      <View className="flex flex-1 flex-col items-center justify-center gap-2">
+        <Text>Current theme: {themeName}</Text>
+        <Pressable onPress={setTheme} className="bg-primary border px-4 py-2">
+          <Text className="text-neutral-accent">
+            Set {themeName === 'light' ? 'dark' : 'light'} theme
+          </Text>
+        </Pressable>
       </View>
     </>
   );
