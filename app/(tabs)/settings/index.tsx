b31800077932efea2fbb93f4361e414b2b9cbd2a
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-12-05 18:11:50 -0800

    Make settings page look better

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, Text, Alert } from 'react-native';
 import { Stack } from 'expo-router';
 import { useThemeContext } from '@/store/context/themeContext';
-import Button from '@/components/ui/MyButton';
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import { getActionSheetStyle } from '@/lib/actionSheetConfig';
 import { deleteWorkouts } from '@/db/reset';
+import Button from '@/components/ui/MyButton';
 
 export default function SettingsTab() {
   const { colors, themeName, setTheme } = useThemeContext();
@@ -47,20 +47,30 @@ export default function SettingsTab() {
   return (
     <>
       <Stack.Screen options={{ title: 'Settings' }} />
-      <View className="flex-1 bg-neutral p-2">
-        <Text className="text-xl text-neutral-contrast">
-          Current Theme: {themeName}
-        </Text>
-        <Button onPress={setTheme}>
-          <Text className="text-center text-xl text-white">
-            Set {themeName === 'light' ? 'dark' : 'light'} theme
+      <View className="flex flex-1 flex-col gap-2 bg-neutral p-2">
+        <View className="flex flex-row items-center gap-2">
+          <Text className="w-40 text-xl font-medium text-neutral-contrast">
+            Theme Settings
           </Text>
-        </Button>
-        <Button onPress={handleDeleteWorkouts}>
-          <Text className="text-center text-xl text-neutral-contrast">
-            Delete All Previous Workouts
+          <Button onPress={setTheme} className="flex-1 bg-neutral-accent">
+            <Text className="text-center text-xl text-neutral-contrast">
+              Set {themeName === 'light' ? 'Dark' : 'Light'} Theme
+            </Text>
+          </Button>
+        </View>
+        <View className="flex flex-row items-center gap-2">
+          <Text className="w-40 text-xl font-medium text-neutral-contrast">
+            Workout Settings
           </Text>
-        </Button>
+          <Button
+            onPress={handleDeleteWorkouts}
+            className="flex-1 bg-neutral-accent"
+          >
+            <Text className="text-center text-xl text-neutral-contrast">
+              Delete All Previous Workouts
+            </Text>
+          </Button>
+        </View>
       </View>
     </>
   );
