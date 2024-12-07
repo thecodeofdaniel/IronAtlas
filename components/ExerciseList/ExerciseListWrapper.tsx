06473d40cf7d9f169fd74705cdb1d7a40f1e34d8
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-12-07 03:51:31 -0800

    Add option to create exercises if exercises list is empty

--- components/ExerciseList/ExerciseListWrapper.tsx
+++ components/ExerciseList/ExerciseListWrapper.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { FilterExerciseStateVal } from '@/store/zustand/filterExercises/filterExercisesStore';
-import { ExerciseStateVal } from '@/store/zustand/exercise/exerciseStore';
+import {
+  type ExerciseStateVal,
+  type ExerciseStateFunctions,
+  useExerciseStoreHook,
+} from '@/store/zustand/exercise/exerciseStore';
 import TextContrast from '@/components/ui/TextContrast';
 import MyButtonOpacity from '@/components/ui/MyButtonOpacity';
+import { exercises } from '@/db/seed/data';
+import { formatTagOrExercise } from '@/utils/utils';
+
+async function createExercises(func: ExerciseStateFunctions['createExercise']) {
+  for (const [index, exercise] of exercises.entries()) {
+    await func(
+      {
+        label: exercise.label,
+        value: formatTagOrExercise(exercise.label),
+        index: index,
+      },
+      [],
+    );
+  }
+}
 
 type Props = {
   selectedTags: FilterExerciseStateVal['selectedTags'];
@@ -20,12 +39,16 @@ export default function ExerciseListWrapper({
   children,
   className,
 }: Props) {
+  const { setter } = useExerciseStoreHook();
+
   return (
     <>
       {selectedTags.length === 0 && exercisesList.length === 0 && (
         <View className="flex-1 items-center justify-center">
           <TextContrast>No Exercises Found</TextContrast>
-          <MyButtonOpacity>
+          <MyButtonOpacity
+            onPress={() => createExercises(setter.createExercise)}
+          >
             <Text className="font-medium text-white">Add Exercises</Text>
           </MyButtonOpacity>
         </View>
