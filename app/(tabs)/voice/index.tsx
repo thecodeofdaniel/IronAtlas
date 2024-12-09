6297a3e992a880485650dd946f3f97739c43abde
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-12-08 20:51:31 -0800

    Add voice input capability

--- app/(tabs)/voice/index.tsx
+++ app/(tabs)/voice/index.tsx
@@ -1,11 +1,55 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import ScreenLayoutWrapper from '@/components/ui/ScreenLayoutWrapper';
+import { View, Text, Button, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ExpoSpeechRecognitionModule,
+  useSpeechRecognitionEvent,
+} from 'expo-speech-recognition';
 
 export default function VoiceTab() {
+  const [recognizing, setRecognizing] = useState(false);
+  const [transcript, setTranscript] = useState('');
+
+  useSpeechRecognitionEvent('start', () => setRecognizing(true));
+  useSpeechRecognitionEvent('end', () => setRecognizing(false));
+  useSpeechRecognitionEvent('result', (event) => {
+    setTranscript(event.results[0]?.transcript);
+  });
+  useSpeechRecognitionEvent('error', (event) => {
+    console.log('error code:', event.error, 'error message:', event.message);
+  });
+
+  const handleStart = async () => {
+    const result = await ExpoSpeechRecognitionModule.requestPermissionsAsync();
+    if (!result.granted) {
+      console.warn('Permissions not granted', result);
+      return;
+    }
+    // Start speech recognition
+    ExpoSpeechRecognitionModule.start({
+      lang: 'en-US',
+      interimResults: true,
+      maxAlternatives: 1,
+      continuous: false,
+      requiresOnDeviceRecognition: false,
+      addsPunctuation: false,
+      contextualStrings: ['Carlsen', 'Nepomniachtchi', 'Praggnanandhaa'],
+    });
+  };
+
   return (
-    <ScreenLayoutWrapper>
-      <Text>VoiceTab</Text>
-    </ScreenLayoutWrapper>
+    <View>
+      {!recognizing ? (
+        <Button title="Start" onPress={handleStart} />
+      ) : (
+        <Button
+          title="Stop"
+          onPress={() => ExpoSpeechRecognitionModule.stop()}
+        />
+      )}
+
+      <ScrollView>
+        <Text>{transcript}</Text>
+      </ScrollView>
+    </View>
   );
 }
