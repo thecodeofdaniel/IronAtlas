e62d3d1fd134a5389e8e71e767397fe8330a2339
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-12-08 19:49:09 -0800

    Add package for speech-to-text

---
+++ app/(tabs)/voice/_layout.tsx
@@ -0,0 +1,5 @@
+import HeaderLayout from '@/components/ui/HeaderLayout';
+
+export default function Layout() {
+  return <HeaderLayout />;
+}
---
+++ app/(tabs)/voice/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import ScreenLayoutWrapper from '@/components/ui/ScreenLayoutWrapper';
+
+export default function VoiceTab() {
+  return (
+    <ScreenLayoutWrapper>
+      <Text>VoiceTab</Text>
+    </ScreenLayoutWrapper>
+  );
+}
